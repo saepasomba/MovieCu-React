93815c755edee4e151ccade04483dbd822675136
Sae Pasomba <saepasomba12@gmail.com>
2022-10-05 14:38:15 +0700

    Fix minor layouting on header carousel

--- src/components/CustomCarousel/CustomCarousel.js
+++ src/components/CustomCarousel/CustomCarousel.js
@@ -18,10 +18,12 @@ export default function CustomCarousel({ movies }) {
             return (
               <div key={movie.id} className='carousel-item'>
                 <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt='movie backdrop' />
-                <div className='carousel-item-details'>
-                  <h1>{movie.title ? movie.title : movie.name}</h1>
-                  <p>{movie.overview}</p>
-                  <CustomButton icon={<AiOutlinePlayCircle />} text={'WATCH TRAILER'} />
+                <div className='container container-carousel-details'>
+                  <div className='carousel-item-details'>
+                    <h1>{movie.title ? movie.title : movie.name}</h1>
+                    <p>{movie.overview}</p>
+                    <CustomButton icon={<AiOutlinePlayCircle />} text={'WATCH TRAILER'} />
+                  </div>
                 </div>
               </div>
             )
