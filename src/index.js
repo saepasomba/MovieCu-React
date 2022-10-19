8559181be23c616b315c796e1768eddc43e1355f
Sae Pasomba <saepasomba12@gmail.com>
2022-10-19 20:38:46 +0700

    Add google oauth with latest dependency

--- src/index.js
+++ src/index.js
@@ -9,21 +9,25 @@ import MovieSearch from './pages/MovieSearch/MovieSearch';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/details/:movieID' element={<MovieDetails />} />
-        <Route path='/search/:query' element={<MovieSearch action='search' />} replace />
-        <Route path='/genre/:query' element={<MovieSearch action='genre' />} replace />
-        <Route path='*' element={<Navigate to="/" />} />
-      </Routes>
-    <Footer />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="183081395657-c2mte3r3jqrdiupte3u0icne6ebnpeop.apps.googleusercontent.com">
+    <React.StrictMode>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/details/:movieID' element={<MovieDetails />} />
+          <Route path='/search/:query' element={<MovieSearch action='search' />} replace />
+          <Route path='/genre/:query' element={<MovieSearch action='genre' />} replace />
+          <Route path='*' element={<Navigate to="/" />} />
+        </Routes>
+      <Footer />
+      </BrowserRouter>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
